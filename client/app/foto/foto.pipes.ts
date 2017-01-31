55223eb2d75345fb88ac37805a86357843ae5ece
alexnfsc175 <alex_3_do@outlook.com>
2017-01-31 21:56:11 -0200

    Angular 2 - Fim Modulo 1

--- client/app/foto/foto.pipes.ts
+++ client/app/foto/foto.pipes.ts
@@ -1,12 +1,14 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import { FotoComponent } from './foto.component'
 
 @Pipe({
     name: 'filtroPorTitulo'
 })
 export class FiltroPorTitulo implements PipeTransform { 
 
-    transform(fotos, digitado){
+    transform(fotos: FotoComponent[] , digitado: string){
 
-        return fotos.filter( foto => foto.titulo.toLowerCase().includes(digitado.toLowerCase()));
+        digitado = digitado.toLowerCase();
+        return fotos.filter( foto => foto.titulo.toLowerCase().includes(digitado));
     }
 }
