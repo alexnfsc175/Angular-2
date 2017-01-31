3003ce3bd55e9fd2852bfba3eef64e7740dec0a1
alexnfsc175 <alex_3_do@outlook.com>
2017-01-31 18:12:18 -0200

    ignore

---
+++ README.md
@@ -0,0 +1,4 @@
+# Angular2-Alurapic
+Curso da Alura sobre Angular 2, projeto Alurapic.
+
+fazer isso node_modules\.bin\typings install
---
+++ client/app/app.component.ts
@@ -0,0 +1,22 @@
+import {Component} from '@angular/core';
+import { Http } from '@angular/http';
+
+@Component({
+    moduleId: module.id,
+    selector: 'app',
+    templateUrl: './app.component.html'
+})
+export class AppComponent {
+
+    fotos: Array<Object> = [];
+
+    constructor(http: Http){
+        let stream = http.get('v1/fotos');
+
+        stream.subscribe(res => {
+
+          this.fotos = res.json();  
+          console.log(this.fotos);
+        });
+    }
+} 
